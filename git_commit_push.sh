cdefdfbb4f5bf2068359b86afb8de8dc0af214ea
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-17 22:17:03 +0300

    feat: Add Sidebar component
    This commit introduces the Sidebar component, which serves as the navigation sidebar for the application. The Sidebar component includes a logo that links back to the home page.
    
    Changes:
    - Added Sidebar component to the project.
    - Utilized the Next.js Image component for optimized image loading.
    - Implemented a Link component from Next.js for navigation.
    - Integrated the sidebar-logo class for styling consistency.
    
    Files modified:
    - Added Sidebar.tsx to components directory.
    
    Images added:
    - Added logo-text.svg to the assets/images directory.
    
    This Sidebar component enhances the user experience by providing easy navigation within the application.

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,7 +5,24 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-feat: Add signin and signup pages
+feat: Add Sidebar component
+
+This commit introduces the Sidebar component, which serves as the navigation sidebar for the application. The Sidebar component includes a logo that links back to the home page.
+
+Changes:
+- Added Sidebar component to the project.
+- Utilized the Next.js Image component for optimized image loading.
+- Implemented a Link component from Next.js for navigation.
+- Integrated the sidebar-logo class for styling consistency.
+
+Files modified:
+- Added Sidebar.tsx to components directory.
+
+Images added:
+- Added logo-text.svg to the assets/images directory.
+
+This Sidebar component enhances the user experience by providing easy navigation within the application.
+
 "
 # Push changes to the remote repository
 git push origin HEAD
