8fc1cf70bbdc23513d99d180faa52de7dd2ced62
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-17 20:43:55 +0300

    feat: Add Tailwind CSS customizations and utility classes

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,7 +5,7 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-feat: Add Tailwind CSS configuration with customizations and plugins
+feat: Add Tailwind CSS customizations and utility classes
 
 "
 # Push changes to the remote repository
