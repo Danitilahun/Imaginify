f788eeb68e7080adf01170eeee34b237dd97abe5
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-17 23:46:57 +0300

    chore(types): Add type declarations for various parameters
    This commit adds type declarations for various parameters used throughout the application. These type declarations provide clarity and improve the maintainability of the codebase by documenting the expected structure of different data objects.
    
    Changes:
    - Added type declarations for CreateUserParams, UpdateUserParams, AddImageParams, UpdateImageParams, Transformations, CheckoutTransactionParams, CreateTransactionParams, TransformationTypeKey, FormUrlQueryParams, UrlQueryParams, RemoveUrlQueryParams, SearchParamProps, TransformationFormProps, and TransformedImageProps.
    
    These type declarations enhance the readability of the code and help prevent potential bugs by enforcing strict type checking.
    
    Files modified:
    - Added type declarations for various parameters in the codebase.

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,20 +5,17 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-feat: Add Mongoose model for Transaction documents
+chore(types): Add type declarations for various parameters
 
-This commit introduces a Mongoose model to represent Transaction documents in the MongoDB database. The model defines the schema for Transaction documents and exports it for use in other parts of the application.
+This commit adds type declarations for various parameters used throughout the application. These type declarations provide clarity and improve the maintainability of the codebase by documenting the expected structure of different data objects.
 
 Changes:
-- Defined a Mongoose schema for Transaction documents with fields representing various attributes of a transaction.
-- Included fields such as createdAt, stripeId, amount, plan, credits, and buyer in the schema.
-- Applied unique constraint for stripeId to ensure its uniqueness.
-- Implemented a model named Transaction to interact with the MongoDB collection for Transaction documents, preventing redefinition if already defined.
+- Added type declarations for CreateUserParams, UpdateUserParams, AddImageParams, UpdateImageParams, Transformations, CheckoutTransactionParams, CreateTransactionParams, TransformationTypeKey, FormUrlQueryParams, UrlQueryParams, RemoveUrlQueryParams, SearchParamProps, TransformationFormProps, and TransformedImageProps.
 
-This addition facilitates the storage and retrieval of Transaction documents from the MongoDB database, providing structured data management for transactions in the application.
+These type declarations enhance the readability of the code and help prevent potential bugs by enforcing strict type checking.
 
 Files modified:
-- Created a new file named transaction.ts to define the Mongoose model for Transaction documents.
+- Added type declarations for various parameters in the codebase.
 
 "
 # Push changes to the remote repository
