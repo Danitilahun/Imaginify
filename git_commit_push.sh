9637cfa5af16c667b35611ccc586e912358945af
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-17 22:20:01 +0300

    feat: Add constants and configurations
    This commit introduces various constants and configurations essential for the application's functionality. These include navigation links, subscription plans, transformation types, aspect ratio options, default form values, and credit fee.
    
    Changes:
    - Added constants for navigation links, plans, transformation types, aspect ratio options, default form values, and credit fee.
    
    These constants and configurations are crucial for defining the behavior and options available throughout the application.
    
    Files modified:
    - Added constants.ts file containing the defined constants and configurations.

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,23 +5,17 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-feat: Add Sidebar component
+feat: Add constants and configurations
 
-This commit introduces the Sidebar component, which serves as the navigation sidebar for the application. The Sidebar component includes a logo that links back to the home page.
+This commit introduces various constants and configurations essential for the application's functionality. These include navigation links, subscription plans, transformation types, aspect ratio options, default form values, and credit fee.
 
 Changes:
-- Added Sidebar component to the project.
-- Utilized the Next.js Image component for optimized image loading.
-- Implemented a Link component from Next.js for navigation.
-- Integrated the sidebar-logo class for styling consistency.
+- Added constants for navigation links, plans, transformation types, aspect ratio options, default form values, and credit fee.
+
+These constants and configurations are crucial for defining the behavior and options available throughout the application.
 
 Files modified:
-- Added Sidebar.tsx to components directory.
-
-Images added:
-- Added logo-text.svg to the assets/images directory.
-
-This Sidebar component enhances the user experience by providing easy navigation within the application.
+- Added constants.ts file containing the defined constants and configurations.
 
 "
 # Push changes to the remote repository
