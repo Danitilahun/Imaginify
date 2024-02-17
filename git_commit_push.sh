0d63f97e1d66bf5b1f23cc8ce90cf92d5e06bb43
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-17 21:14:18 +0300

    feat: change main image position

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,7 +5,7 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-feat: Add Layout component
+feat: change main image position
 "
 # Push changes to the remote repository
 git push origin HEAD
