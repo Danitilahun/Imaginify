86d5a39d56d4cb7c2a21797dae8126426f565e66
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-21 21:27:55 +0300

    feat: Add pagination and search functionality to Collection component
    - Added pagination controls to navigate through pages of images
    - Implemented search functionality when  prop is true
    - Updated Collection component to display recent edits
    - Added Card component to render individual image cards with transformation icons

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,7 +5,13 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-feat: Implement delete confirmation functionality in DeleteConfirmation component
+feat: Add pagination and search functionality to Collection component
+
+- Added pagination controls to navigate through pages of images
+- Implemented search functionality when `hasSearch` prop is true
+- Updated Collection component to display recent edits
+- Added Card component to render individual image cards with transformation icons
+
 "
 # Push changes to the remote repository
 git push origin HEAD
