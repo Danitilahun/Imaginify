3b49cf0da2d99cf06b789795a2059327b5b67539
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-23 15:57:37 +0300

    feat: adding api data for clerk and stripe

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -5,14 +5,7 @@ git add .
 
 # Commit changes with the specified commit message
 git commit -m "
-feat: Implement Toaster component for displaying toast notifications
-
-- Created Toaster component to render toast notifications using custom UI components
-- Utilized useToast hook to manage toast state and retrieve toast data
-- Implemented mapping over toast data to render individual toast components dynamically
-- Integrated ToastProvider to manage the toast rendering context
-- Used ToastViewport component for controlling toast viewport behavior
-
+feat: adding api data for clerk and stripe
 "
 # Push changes to the remote repository
 git push origin HEAD
